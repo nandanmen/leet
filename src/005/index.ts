ecefe1ef0adbd6ebe62b9e393df98f7aa9c4598d
narendrasss <n.syahrasyad.n@gmail.com>
2019-07-09 14:10:59 -0700

    Implemented #005 with DP

--- src/005/index.ts
+++ src/005/index.ts
@@ -1,3 +1,79 @@
+function isPalindrome(s: string) {
+  let hi = s.length - 1
+  let lo = 0
+  while (lo < hi) {
+    if (s[hi--] !== s[lo++]) {
+      return false
+    }
+  }
+  return true
+}
+
+/**
+ * Given a string s, finds the longest palindrome in s
+ * using brute force.
+ * @summary
+ *  - Runtime: `O(n^3)`
+ *  - Leetcode stats: Time limit exceeded
+ */
+export function longestPalindromeNonOptimal(s: string) {
+  if (s.length <= 1) return s
+
+  let longestSoFar = ''
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      const substr = s.slice(i, j + 1)
+      if (isPalindrome(substr) && substr.length > longestSoFar.length) {
+        longestSoFar = substr
+      }
+    }
+  }
+  return longestSoFar.length ? longestSoFar : s[0]
+}
+
+function makeTableFromString(s: string) {
+  const table: boolean[][] = Array.from({ length: s.length }, () =>
+    new Array(s.length).fill('')
+  )
+
+  for (let i = 0; i < s.length; i++) {
+    table[i][i] = true
+    if (i < s.length - 1) {
+      table[i][i + 1] = s[i] === s[i + 1]
+    }
+  }
+
+  for (let i = s.length - 3; i >= 0; i--) {
+    for (let j = i + 2; j < s.length; j++) {
+      table[i][j] = table[i + 1][j - 1] && s[i] === s[j]
+    }
+  }
+
+  return table
+}
+
+/**
+ * Given a string s, finds the longest palindrome in s
+ * using dynamic programming.
+ * @summary
+ *  - Runtime: `O(n^2)`
+ *  - Memory: `O(n^2)`
+ *  - Leetcode stats:
+ *    - Runtime: `492ms` (16.37 percentile)
+ *    - Memory: `128.1mb` (5.45 percentile)
+ */
 export default function longestPalindrome(s: string) {
-  return s
+  const table = makeTableFromString(s)
+
+  let result = ''
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      const substr = s.slice(i, j + 1)
+      if (table[i][j] && substr.length > result.length) {
+        result = substr
+      }
+    }
+  }
+
+  return result
 }
--- src/005/longest-palindromic-substring.test.ts
+++ src/005/longest-palindromic-substring.test.ts
@@ -1,6 +1,14 @@
 import longestPalindrome from '.'
 
-const cases = [['babad', 'bab'], ['cbbd', 'bb']]
+const cases = [
+  ['', ''],
+  ['a', 'a'],
+  ['ac', 'a'],
+  ['babad', 'bab'],
+  ['cbbd', 'bb'],
+  ['abadd', 'aba'],
+  ['cababac', 'cababac']
+]
 
 describe('longest palindrome', () => {
   test.each(cases)(`given %s, returns %s`, (input, expected) => {
